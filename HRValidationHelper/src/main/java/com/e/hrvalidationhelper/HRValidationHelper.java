b1432adeaa4773b040e93e3f75067357ee640a3c
Ankita singh <ankita.2020@tecorb.co>
2022-11-08 13:42:41 +0530

    change

--- HRValidationHelper/src/main/java/com/e/hrvalidationhelper/HRValidationHelper.java
+++ HRValidationHelper/src/main/java/com/e/hrvalidationhelper/HRValidationHelper.java
@@ -2,19 +2,23 @@ package com.e.hrvalidationhelper;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.text.InputFilter;
 import android.text.Spanned;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 
+import java.util.List;
+import java.util.Locale;
 import java.util.regex.Pattern;
 
 public class HRValidationHelper {
 
-    private  String countryCode;
 
     public static boolean isValidEmail(String email){
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
@@ -47,34 +51,28 @@ public class HRValidationHelper {
             return input;
         }
     }
-    public static int setDimension(View view, Context context, int width, int height) {
-        DisplayMetrics displayMetrics =new  DisplayMetrics();
-        ((Activity)context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        view.getLayoutParams().width = (int)(displayMetrics.widthPixels * width);
-        view.getLayoutParams().height=(int)(displayMetrics.heightPixels* height);
-        return view.getLayoutParams().width;
-    }
 
-    public static int setWidth(View view, Context context, int width) {
+    public static int setScreenWidth(View view, Context context, int width) {
         DisplayMetrics displayMetrics =new  DisplayMetrics();
         ((Activity)context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         view.getLayoutParams().width = (int)(displayMetrics.widthPixels * width);
         return view.getLayoutParams().width;
     }
 
-    public static int setHeight(View view, Context context, int height) {
+    public static int setScreenHeight(View view, Context context, int height) {
         DisplayMetrics displayMetrics =new  DisplayMetrics();
         ((Activity)context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         view.getLayoutParams().height = (int)(displayMetrics.heightPixels * height);
         return view.getLayoutParams().height;
     }
 
+
     public static  void setEditTextMaxLength(final EditText editText, int length) {
         InputFilter[] FilterArray = new InputFilter[1];
         FilterArray[0] = new InputFilter.LengthFilter(length);
         editText.setFilters(FilterArray);
     }
-
+//This method is not show the emoji in the edittext
     public static void setDisableEmojiInTitle(final EditText editText) {
         InputFilter emojiFilter = new InputFilter() {
             @Override
@@ -91,4 +89,60 @@ public class HRValidationHelper {
         };
         editText.setFilters(new InputFilter[]{ emojiFilter });
     }
+
+    //This method help to find tha address of using lat and lng
+    public static String getCompleteAddressString(Context context, double LATITUDE, double LONGITUDE) {
+        String strAdd = "";
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(LATITUDE, LONGITUDE, 10);
+            if (addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("");
+
+                for (int i = 0; i <= returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+                }
+                strAdd = strReturnedAddress.toString();
+                Log.i("Address=========>", strReturnedAddress.toString());
+            } else {
+                Log.i("Address=======>", "No Address returned!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("Address:========>", "Can not get Address!");
+        }
+        return strAdd;
+    }
+    public static String getCurrentPostalCode(Context context, double LATITUDE, double LONGITUDE) {
+        String strAdd = "";
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(LATITUDE, LONGITUDE, 1);
+            if (addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("");
+
+                for (int i = 0; i <= returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress.append(returnedAddress.getPostalCode());
+                }
+                strAdd = strReturnedAddress.toString();
+                Log.i("Postal code=========>", strReturnedAddress.toString());
+            } else {
+                Log.i("Postal code=======>", "No postal code returned!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("Postal code:========>", "Can not get postal code!");
+        }
+        return strAdd;
+    }
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/e/projectlibrary/MainActivity.java
+++ app/src/main/java/com/e/projectlibrary/MainActivity.java
@@ -1,41 +1,52 @@
 package com.e.projectlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.e.hrvalidationhelper.HRValidationHelper;
 
+
+
 public class MainActivity extends AppCompatActivity {
     private Button btn;
-    private EditText email;
+    private EditText email,emoji,maxLength;
     private Context context;
+    private String address,postalCode,emojiHide;
+   private TextView txtAddress,postal;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_main);
         context=this;
         btn=findViewById(R.id.btn);
         email=findViewById(R.id.email);
-        HRValidationHelper.setDisableEmojiInTitle(email);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-              /*
-                if(HRValidationHelper.isValidEmail(email.getText().toString())){
-                    Toast.makeText(context, "success", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(context,"Not valid email",Toast.LENGTH_SHORT).show();
-                }*/
-
-            }
-        });
+        txtAddress=findViewById(R.id.address);
+        postal=findViewById(R.id.postal_code);
+        emoji=findViewById(R.id.emoji_hide);
+        maxLength=findViewById(R.id.max_length);
+        setAllValidation();
     }
+
+
+
+    private void setAllValidation(){
+        address=HRValidationHelper.getCompleteAddressString(context,28.5355,77.3910);
+        postalCode=HRValidationHelper.getCurrentPostalCode(context,25.6924,85.2083);
+        HRValidationHelper.setDisableEmojiInTitle(emoji);
+        HRValidationHelper.setEditTextMaxLength(maxLength,8);
+        txtAddress.setText(address);
+        postal.setText(postalCode);
+    }
+
 }
